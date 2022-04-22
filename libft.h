490c5ce917421b0c8eb5a474d5b28126021526ee
sirhcofe <livefatezz@gmail.com>
2022-04-22 17:27:19 +0800

    fourth commit

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: chenlee <chenlee@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 17:39:58 by chenlee           #+#    #+#             */
-/*   Updated: 2022/04/20 19:43:12 by chenlee          ###   ########.fr       */
+/*   Updated: 2022/04/22 17:26:49 by chenlee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@
 
 void	*ft_bzero(void *s, size_t n);
 void	*ft_memset(void *str, int c, size_t len);
+void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 int		ft_isalnum(int c);
@@ -28,7 +29,10 @@ int		ft_isprint(int c);
 int     ft_toupper(int c);
 int     ft_tolower(int c);
 int		ft_atoi(const char *str);
-char    *ft_strchr(const char *s, int c)
+int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char    *ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
 size_t	ft_strlen(const char *s);
 size_t	strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
